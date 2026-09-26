3e9c97d7d3ffc8ac677738d10bd66896462e42d1
Ayodele Oluwaseyi <seyiayodele006@gmail.com>
2026-09-26 09:42:25 +0100

    fixed nullable issue

--- app/models/notifications.py
+++ app/models/notifications.py
@@ -10,7 +10,7 @@ class Notification(Base):
     __tablename__ = "notifications"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     announcement_id = Column(UUID(as_uuid=True), ForeignKey("announcements.id"))
     is_read = Column(Boolean, default=False)
     created_at = Column(DateTime, default=datetime.now)
