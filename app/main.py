64f7812b19ee2690b63a33818ceed0b994672ae3
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-09-26 15:10:48 +0100

    fix: i added the respective routes to the main.py

--- app/main.py
+++ app/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 
 # from app.db.session import Base, engine
 from app.models import user, group, keyword, announcement
-from app.api.routes import webhook, auth, keywords, admin_groups, admin_announcements, admin_analytics
-
+from app.api.routes import (
+    webhook, auth, keywords, admin_groups,
+    admin_announcements, admin_analytics,
+    groups, announcements, notification,
+)
 app = FastAPI(title="Centralized Student Information System")
 # Base.metadata.create_all(bind=engine)
 
@@ -17,3 +20,6 @@ app.include_router(keywords.router, prefix="/api/v1")
 app.include_router(admin_groups.router, prefix="/api/v1")
 app.include_router(admin_announcements.router, prefix="/api/v1")
 app.include_router(admin_analytics.router, prefix="/api/v1")
+app.include_router(groups.router)
+app.include_router(announcements.router)
+app.include_router(notification.router)
