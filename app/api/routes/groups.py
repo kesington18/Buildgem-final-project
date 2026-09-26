c351699a955ea20de69882e117bad66a8ec3a494
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-09-26 15:02:47 +0100

    fix: i change the groups.py to return only the groups that are active that is where the bot is allowed to read the messsages.

--- app/api/routes/groups.py
+++ app/api/routes/groups.py
@@ -12,5 +12,4 @@ def get_groups(
         current_user: User = Depends(get_current_user),
         db: Session = Depends(get_db)
 ):
-    groups = db.query(TelegramGroup).all()
-    return groups
+    return db.query(TelegramGroup).filter(TelegramGroup.is_active.is_(True)).all()
